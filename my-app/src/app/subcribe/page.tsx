36a90b4b3e76813ba7afc1a87bc2c9c9b1ab7384
roopika-19 <154577207+roopika-19@users.noreply.github.com>
2024-08-14 17:56:05 +0530

    Update page.tsx

--- my-app/src/app/subcribe/page.tsx
+++ my-app/src/app/subcribe/page.tsx
@@ -10,7 +10,7 @@ const SubscribePage = () => {
       <img src="https://68.media.tumblr.com/abc925865de6e5f091dc7721315db6ea/tumblr_inline_orpfllNWRK1v00f4n_540.png" alt="Subscribe" />
       <h2>Subscribe</h2>
       <form >
-        <button onClick={()=>router.push('https://accounts.google.com/o/oauth2/v2/auth?client_id=367239988199-rmkv4odnep7tvn89knda50btbf0opvri.apps.googleusercontent.com&redirect_uri=http://localhost:3000/auth/callback/google&response_type=code&include_granted_scopes=true&access_type=offline&scope=https%3A//www.googleapis.com/auth/gmail.readonly')}>Subscribe</button>
+        <button onClick={()=>router.push('https://accounts.google.com/o/oauth2/v2/auth?client_id=3672399-rmkv4odnep7tvn89knda50btbf0opvri.apps.googleusercontent.com&redirect_uri=http://localhost:3000/auth/callback/google&response_type=code&include_granted_scopes=true&access_type=offline&scope=https%3A//www.googleapis.com/auth/gmail.readonly')}>Subscribe</button>
       </form>
     </div>
   );
